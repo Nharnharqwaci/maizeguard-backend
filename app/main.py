4e6c2b5af5a843f24c82eaa1cae20e7f29ff7c63
Jonathan <jonathanaboagye66@gmail.com>
2026-07-27 18:44:26 +0000

    Fix CORS

--- app/main.py
+++ app/main.py
@@ -33,7 +33,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
-        "https://maizeguard-frontend-8772.vercel.app/"
+        "https://maizeguard-frontend-8772.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
